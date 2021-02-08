80dde8bf639252735a53dc3bd1b532b37fef6885
Rickson Turra <rickynhoct@gmail.com>
2021-02-08 13:45:26 -0300

    Created a create.js file to prevent form default submit on create.html. Updated create view to save form data to database.

--- nova_project/nova_app/views.py
+++ nova_project/nova_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django import forms
 from .models import Events
 from .forms import EventForm
@@ -12,14 +12,17 @@ def index(request):
     return render(request, "nova_app/index.html")
 
 def create(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("nova_app:index"))
+    else:
+        form = EventForm()
+
     return render(request, "nova_app/create.html", {
-        "form": EventForm(),
+        "form": form,
     })
 
 
-# class DateInput(forms.DateInput):
-#     input_type = 'date'
-
-# class TemplateForm(forms.Form):
-#     my_date_field = forms.DateField(label="Choose Date", widget=DateInput)
 
