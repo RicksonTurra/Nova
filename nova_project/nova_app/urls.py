df9ad764610423c57e8b4e8eb27f73a7d02dd815
Rickson Turra <rickynhoct@gmail.com>
2021-02-09 02:08:31 -0300

    Added new url for people to check each event

--- nova_project/nova_app/urls.py
+++ nova_project/nova_app/urls.py
@@ -6,5 +6,6 @@ app_name = "nova_app"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("create", views.create, name="create")
+    path("create", views.create, name="create"),
+    path("redeem/<str:ticketIdentifier>", views.check, name="event")
 ]
