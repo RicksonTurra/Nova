8ee7a0b5aa7a4c33cf62a7daccb900817f3181ac
Rickson Turra <rickynhoct@gmail.com>
2021-02-12 19:46:16 -0300

    Added a few comments to the urls file to provide a better understanding of the paths

--- nova_project/nova_app/urls.py
+++ nova_project/nova_app/urls.py
@@ -7,14 +7,18 @@ app_name = "nova_app"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
+    # Checks a specific event
     path("event/<int:event_pk>", views.check, name="event"),
     path("show", views.show_events, name="show"),
+    # Checks all events
     path("events", views.show_all, name="show_all"),
     path("event/refresh/<str:id_event>", views.refresh, name="refresh"),
     # Endpoint to check the status of a ticket - returns a JSON.
     path("status/response/<str:tokenID>", views.ticket_status, name="status"),
     # Endpoint that renders a ticket status
     path("status/<str:IDtoken>", views.check_status, name="check_status"),
+    # Redeems a ticket
     path("redeem/<str:token>", views.redeem, name="redeem"),
+    # Adds new tickets
     path("add", views.add_ticket, name="add")
 ]
